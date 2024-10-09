f142f587e88d12c609e8bf5e9cb7ed38b591e653
Brandon Norman <brandonnorman@gmail.com>
2024-10-08 20:23:26 -0700

    Delete hw04add.cpp

--- hw04add.cpp
+++
@@ -1,9 +0,0 @@
-#include "hw04.hpp"
-
-void addMatrices(int** A, int** B, int rows, int cols, int** result) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            result[i][j] = A[i][j] + B[i][j];
-        }
-    }
-}
